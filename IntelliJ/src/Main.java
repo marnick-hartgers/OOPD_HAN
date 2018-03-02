502708c2041d39c8e90df3b167c54c2730d205da
Marnick Hartgers <marnick.hartgers@gmail.com>
2018-03-02 14:46:55 +0100

    again

--- IntelliJ/src/Main.java
+++ IntelliJ/src/Main.java
@@ -1,10 +1,10 @@
 import processing.core.PApplet;
-
+import Les9StudieChecker.Les9StudieChecker;
 
 public class Main extends PApplet {
     public static void main(String[] args) {
-        PApplet.main("LottoMachine.Window");
-
+        //PApplet.main("LottoMachine.Window");
+        Les9StudieChecker.main(args);
     }
 
 
